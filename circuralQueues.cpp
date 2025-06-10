f2170f065962dfcb2a430f4ac8c57dc369a5a741
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-10 11:09:48 +0700

    cek apakah antrian kosong

--- circuralQueues.cpp
+++ circuralQueues.cpp
@@ -46,7 +46,12 @@ public:
 
     void remove () 
     {
-        
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
         
     }
     
