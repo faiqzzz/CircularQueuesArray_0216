574b1bb4c1c4fca823a40354c31e9b9631c211d3
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-10 11:37:36 +0700

    program utama

--- circuralQueues.cpp
+++ circuralQueues.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Queues 
 {
 private:
-    int FRONT, REAR, max = 5;
+    static const int max = 5;
+    int FRONT, REAR;
     int queue_array[5];
 
 public:
@@ -98,5 +99,60 @@ public:
     }
 
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4)";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4':
+                {
+                    return 0;
+                }
+                default:
+                {
+                    cout << "Invalid option!!" << endl;
+                    break;
+                }
+                
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+        
+        
+    }
+}
 
 
