d7e92d28138e4967b8f54e0ab4eced1308f13366
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-10 11:10:10 +0700

    Cek jika antrian hanya memiliki satu elemen

--- circuralQueues.cpp
+++ circuralQueues.cpp
@@ -52,7 +52,22 @@ public:
             cout << "Queue underflow\n";
             return;
         }
-        
+        cout << "\nThe element deleted from queue is :" << queue_array[FRONT] << "\n";
+
+        // Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) 
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // Jika elemen yang di hapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else    
+                FRONT = FRONT + 1;
+        }
     }
     
 };
